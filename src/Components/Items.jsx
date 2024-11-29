6b73610feb1fe20e200d210577bd2c0b6a336dd4
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-11-29 21:40:43 +0530

    Update Itemcard using map

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ItemsCard from './ItemsCard'
 
 const Items = () => {
+  const [Items,setItems] = useState([])
+
+
+  const fetchData = async () => {
+    const data = await fetch('constantData.json')
+    const json = await data.json()
+    setItems(json.data?.cards || [])
+    // console.log(json?.data?.cards);
+  } 
+  useEffect(() => {
+    fetchData()
+  } ,[])
+
+
   return (
     <div className='flex flex-wrap mx-[15%]'>
-        <ItemsCard name="Chandu" cuisines="milk and powder"/>
-        <ItemsCard name="Chandu" cuisines="milk and powder"/>
-        <ItemsCard name="Chandu" cuisines="milk and powder"/>
-        <ItemsCard name="Chandu" cuisines="milk and powder"/>
-        <ItemsCard name="Chandu" cuisines="milk and powder"/>
-        <ItemsCard name="Chandu" cuisines="milk and powder"/>
-
-   
-
+        {Items.map((item) => (
+          <ItemsCard key={item?.info?.id} data={item?.info}/>
+        ))}
     </div>
   )
 }

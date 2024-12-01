0e697c4858f9dd52ddac9a4e5c81243c181c718c
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-01 20:08:23 +0530

    Add footer component

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import BgSlide from './BgSlide'
 // import ItemsCard from './ItemsCard'
 import Items from './Items'
+import Footer from './Footer'
 
 const Body = () => {
 
@@ -11,6 +12,7 @@ const Body = () => {
         <Header/>
         <BgSlide/>
         <Items/>
+        <Footer/>
     </div>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full bg-gray-200 h-40'>
+        Footer
+    </div>
+  )
+}
+
+export default Footer

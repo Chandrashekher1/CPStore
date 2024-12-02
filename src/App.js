8fb63e7379cba4aaa4aedce9deb85eb6609f1dd4
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-02 14:46:18 +0530

    Add router in the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Body from './Components/Body';
+import Header from './Components/Header';
+import Login from './Components/Login';
+import Cart from './Components/Cart';
 
 function App() {
+  
+  const AppLayout = () => {
+    return (
+      <div className='App'>
+          <Header/>
+          <Outlet/>
+      </div>
+    )
+  }
+
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: (<AppLayout/>),
+      children: [
+        {
+          path: "/",
+          element: <Body/>
+        },
+        {
+          path: "/login",
+          element: <Login/>
+        },
+        {
+          path: "/cart",
+          element: <Cart/>
+        }
+      ]
+    },
+    
+  ])
 
   return (
-    <div className="App">
-
-      <Body/>
-      
-      
-  
+    <div>
+      <RouterProvider router={appRouter}/>
     </div>
   );
 }

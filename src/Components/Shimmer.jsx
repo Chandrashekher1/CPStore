be8626a673e9079971ffe9f08031d99159a0aab9
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-02 14:48:28 +0530

    Add shimmer effect

---
+++ src/Components/Shimmer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='flex flex-wrap ml-60'>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+    </div>
+  )
+}
+
+export default Shimmer

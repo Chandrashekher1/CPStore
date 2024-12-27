7326d62202dbc33c2e110c19b1c55791a7161c25
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-27 23:33:15 +0530

    Add OnlineStatus

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import './App.css';
 import Header from './Components/Header';
@@ -6,47 +5,19 @@ import Body from './Components/Body';
 import Login from './Components/Login';
 import Cart from './Components/Cart';
 import ItemsMenu from './Components/ItemsMenu';
-import ItemsCard from './Components/ItemsCard';
-import Footer from "./Components/Footer";
+import Items from "./Components/Items";
 
-// App Layout Component
 const AppLayout = () => (
-  <div className="App">
+  
+  <div className="overflow-x-hidden font-sans">
     <Header />
     <Body/>
     <Outlet />
-    <Footer/>
-    
+    {/* <Footer/> */}
   </div>
 );
 
-// Component to display a list of cards (ItemList)
-const ItemList = () => {
-  const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/constantData.json");
-        const json = await response.json();
-        setCards(json?.data?.cards || []);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <div className="flex flex-wrap justify-center p-4">
-      {cards.map((card) => (
-        <ItemsCard key={card.id} data={card} />
-      ))}
-    </div>
-  );
-};
-
-// Define router
 const appRouter = createBrowserRouter([
   {
     path: "/",
@@ -54,7 +25,7 @@ const appRouter = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <ItemList />,
+        element: <Items />,
       },
       {
         path: "/login",
@@ -72,7 +43,7 @@ const appRouter = createBrowserRouter([
   },
 ]);
 
-// Main App Component
+
 const App = () => (
   <RouterProvider router={appRouter} />
 );

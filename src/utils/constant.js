7694efbef427c5fd8e59a6e56ee65d47a99e01f6
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-28 13:13:57 +0530

    Add all constant

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,5 @@
-export const Image_URL = "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/"
+export const search_Logo = "https://icons.veryicon.com/png/o/miscellaneous/monochrome-icon-1/search-521.png"
+
+export const BG_Image = "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=2700/layout-engine/2022-05/Group-33704.jpg"
+
+export const Shop_Logo = "https://www.creativefabrica.com/wp-content/uploads/2021/01/21/Shop-Logo-Graphics-7955312-1-1-580x387.jpg"

ad79a5d45dbea5159a20917602d9d7b42774ba1f
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-28 13:11:38 +0530

    Update

--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 
 const Shimmer = () => {
   return (
-    <div className='flex flex-wrap ml-60'>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
-        <div className='w-48 h-60 bg-gray-200 border m-8 rounded-xl'></div>
+    <div className='flex flex-wrap mx-2'>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+        <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
+
+        
     </div>
   )
 }

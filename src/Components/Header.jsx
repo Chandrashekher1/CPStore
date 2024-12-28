08d0f330f375bc229382d2fd3f39e72ef87039a8
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-28 13:09:16 +0530

    Add Icons of navBar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,32 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBagShopping, faHouse, faUser  } from '@fortawesome/free-solid-svg-icons';
+import { Shop_Logo } from '../utils/constant';
+
 
 const Header = () => {
-  
+
+  const navigate = useNavigate()
+
+  const handleLogo = (e) =>{
+    navigate("/")
+  }
 
   return (
-    <div className='flex justify-between p-2 bg-white shadow-md'>
-      <div className='px-6'>
-        <img className='w-16 h-16 cursor-pointer ' src="https://cdn.vectorstock.com/i/1000v/98/50/store-icon-logo-vector-27309850.jpg" alt="logo" />
+    <div className="flex justify-between p-1 sticky top-0 bg-white shadow-md">
+      <div className="">
+        <img
+          className="w-20 h-12 cursor-pointer"
+          src={Shop_Logo}
+          alt="logo"
+          onClick={handleLogo}
+        />
+        {/* <h1>CP03</h1> */}
+      </div>
 
-      </div>
-      <div className='flex py-4'>
-        <span className='flex border h-12 border-black  bg-gray-200 rounded-lg'>
-          <img className='h-6 w-6 m-3' src="https://icons.veryicon.com/png/o/miscellaneous/monochrome-icon-1/search-521.png" alt="" />
-        <input 
-          className='w-96 rounded-lg  border  bg-gray-200 focus:outline-none'
-          placeholder='Search "milk"'
-         type="text" />
-        </span>
-        <button className=' mx-3 px-2 bg-red-600 font-bold text-white rounded-xl'>Search</button>
-      </div>
-      <div className='flex p-4 cursor-pointer'>
-        <Link to={"/"}><h3 className='mx-6 font-bold text-lg'>Home</h3></Link>
-        <Link to={"/cart"}><h3 className='mx-6 font-bold text-lg'>Cart</h3></Link>
-        <Link to={"/login"}><h3 className='mx-6 font-bold text-lg'>Login</h3></Link>
+      <div className="flex p-2 cursor-pointer ml-12">
+        <Link to="/">
+          <h3 className="mx-6 font-bold text-lg">
+            <FontAwesomeIcon icon={faHouse} />
+          </h3>
+        </Link>
+        <Link to="/cart">
+          <h3 className="mx-6 font-bold text-lg">
+          <FontAwesomeIcon icon={faBagShopping} />
+          </h3>
+        </Link>
+        <Link to="/login">
+          <h3 className="mx-6 text-lg flex mt-1">
+          <FontAwesomeIcon icon={faUser} />
+          </h3>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

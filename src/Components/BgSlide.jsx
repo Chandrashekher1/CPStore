0454aa0bd56c33960952af45e240efc5b7fa85b8
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-28 13:06:23 +0530

    Add Social Icons

--- src/Components/BgSlide.jsx
+++ src/Components/BgSlide.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { BG_Image } from '../utils/constant'
 
 const BgSlide = () => {
   return (
     <div className='m-2 flex justify-center '>
         <img 
-            className='rounded-xl w-[75%]'
-            src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=2700/layout-engine/2022-05/Group-33704.jpg" alt="bg-logo" />
+            className='rounded-xl '
+            src={BG_Image} alt="bg-logo" />
     </div>
   )
 }
